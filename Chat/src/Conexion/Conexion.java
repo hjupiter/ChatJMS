1b1be27b40b51a77cfc15e5e3781ee5b02559bed
hjupiter <hjupiter20@gmail.com>
2016-08-21 02:30:32 -0500

    conexion a base de datos

---
+++ Chat/src/Conexion/Conexion.java
@@ -0,0 +1,33 @@
+package Conexion;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Angel
+ */
+public class Conexion {
+    private Connection conexion = null;
+    private static final String cadena = "jdbc:postgresql://localhost:5432/Chat";
+    private static final String user = "postgres";
+    private static final String pass = "root";
+    public Connection Conexion(){
+        try{
+            Class.forName("org.postgresql.Driver");
+            conexion = DriverManager.getConnection(cadena,user,pass);
+            System.out.println(conexion);
+            return conexion;
+        }catch(ClassNotFoundException | SQLException e){
+            return conexion;
+        }
+    }  
+}
